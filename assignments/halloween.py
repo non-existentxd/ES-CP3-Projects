902587e911e591c1a144fddd0ebbbf8f20696d67
non-existentxd <emeraude.sosa@ucas-edu.net>
2024-11-15 00:17:58 +0000

    yunnnnn

--- assignments/halloween.py
+++ assignments/halloween.py
@@ -1,20 +1,47 @@
 class Monster:
-    def __init__(self, name, type, place, attack):
+    def __init__(self, name, origin):
         self.name = name
-        self.type = type
-        self.place = place
-        self.attack = attack
+        self.origin = origin
 
-    def __str__(self):
-        return f"This monster's name is {self.name}, they are {self.type} type of monster, they are from {self.place}, and their method of attack is {self.attack}"
-
-class Wendigo(Monster):
-    return f""
-
-class Banshee(Monster):
-
-
+    def attack(self):
+        return "This monster attacks in a mysterious way!"
+    
 class Vampire(Monster):
+    def __init__(self, name, origin):
+        super().__init__(name, origin)
 
+    def attack(self):
+        return f"{self.name} sinks its fangs into you but they can't because their teeth are weak!"
+    
+class Zombie(Monster):
+    def __init__(self, name, origin):
+        super().__init__(name, origin)
 
+    
+    def attack(self):
+        return f"{self.name} is from wisconsin. He doesn't eat brains he eats flowers"
+    
 class Ogre(Monster):
+    def __init__(self, name, origin):
+        super().__init__(name, origin)
+
+    def attack(self):
+        return f"{self.name} loves his swamo. So stay out of it or he'll start singing HALELUJAH"
+    
+class SlenderMAN(Monster):
+    def __iniT__(self, name, origin):
+        super().__init__(name, origin)
+
+    def attack(self):
+        return f"{self.name} is really tall, he can kill you and he can look like a tree. Be careful!"
+    
+
+vampire = Vampire("Edward Collin", "Forks, Washigton")
+zombie = Zombie("Bob", "France")
+ogre = Ogre("Shrek", "The Swampe")
+slender_man = SlenderMAN("sneederman", "italia")
+
+monster = [vampire, zombie, ogre, slender_man]
+
+for monster in monster:
+    print(f"{monster.name} from {monster.origin} attacks: {monster.attack()}")
